546f0f0af5ca0481e3a9ef6dbb837a17d2d98047
Pierre LE LEZ <ple-lez@e2r8p1.42.fr>
2017-02-03 13:31:16 +0100

    Light on cone

--- sources/light.c
+++ sources/light.c
@@ -6,7 +6,7 @@
 /*   By: ple-lez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 20:36:10 by ple-lez           #+#    #+#             */
-/*   Updated: 2017/02/02 12:22:04 by ple-lez          ###   ########.fr       */
+/*   Updated: 2017/02/03 13:30:56 by ple-lez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ double		lambert(t_obj obj, t_ray ray, t_lgt lgt)
 	t_vec	dir;
 	double	res;
 
+	if (obj.rot)
+		ray.dir = quat_rot(obj.rot, &ray.dir);
 	tmp = vector_scale(ray.dir, ray.t);
 	lgt.hitpnt = vector_add(ray.org, tmp);
 	dir = vector_sub(lgt.hitpnt, lgt.pos);
@@ -40,8 +42,12 @@ double		lambert(t_obj obj, t_ray ray, t_lgt lgt)
 		tmp = vector_sub(obj.pos, lgt.hitpnt);
 		lgt.normal = normalize_vector(tmp);
 	}
+	if (obj.type == CONE)
+		lgt.normal.z *= 2;
 	res = scalar_product(dir, lgt.normal);
 	if (res < 0.1)
 		res = 0.1;
+	if (res > 1)
+		res = 1;
 	return (res);
 }
