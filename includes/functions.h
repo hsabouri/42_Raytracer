bdcf1f4b47b85fbb8bfdf1f8750a673f1d0cefb1
Pierre LE LEZ <ple-lez@e1r5p23.42.fr>
2017-02-06 02:42:58 +0100

    Include Separation

---
+++ includes/functions.h
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ple-lez <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/06 01:37:11 by ple-lez           #+#    #+#             */
+/*   Updated: 2017/02/06 02:30:51 by ple-lez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNCTIONS_H
+# define FUNCTIONS_H
+# include "rt.h"
+
+/* Algorithm Functions */
+
+int			raytrace(t_cam camera, t_obj *objs, t_env env);
+
+/* Vector Functions	*/
+
+t_vec		new_vector(double x, double y, double z);
+t_vec		vector_add(t_vec v1, t_vec v2);
+t_vec		vector_sub(t_vec v1, t_vec v2);
+t_vec		vector_scale(t_vec v1, double k);
+t_vec		normalize_vector(t_vec v);
+t_vec		cross_product(t_vec v1, t_vec v2);
+t_vec		quat_rot(t_quat *q1, t_vec *vec);
+double		scalar_product(t_vec v1, t_vec v2);
+double		get_vector_len(t_vec v);
+void		print_vector(t_vec v);
+
+/* Quaternion functions */
+
+t_quat		new_quat(float radians, t_vec axis);
+t_quat		quat_mult(t_quat *q1, t_quat *q2);
+void		print_quat(t_quat *q1);
+
+/* Camera functions */
+
+t_cam		move_camera(t_cam cam, t_dir dir);
+t_cam		rotate_cam(t_cam cam, t_quat *rot);
+t_cam		translate_cam(t_cam cam, t_vec tran);
+t_cam		camera_control(t_cam cam, t_dir dir);
+
+/* Light functions */
+
+double		lambert(t_obj obj, t_ray ray, t_lgt lgt);
+t_color		apply_lambert(t_color col, double l);
+
+/* Intersection functions */
+
+double		intersect_cone(t_ray *ray, t_obj cone);
+double		intersect_plane(t_ray *ray, t_obj plane);
+double		intersect_sphere(t_ray *ray, t_obj sphere);
+double		intersect_cylinder(t_ray *ray, t_obj cylinder);
+
+/* Math functions */
+
+double		solve_quadra(double a, double b, double c);
+t_vec		normal_plane(t_ray ray, t_obj obj);
+
+/* Initialization functions */
+
+t_cam		init_cam(t_vec pos, t_quat *rot, double fov);
+t_ray		init_ray(t_cam *cam, int x, int y);
+t_obj		*init_objs(void);
+t_env		init_env(void);
+
+/* Mlx hook functions */
+
+void		pixel_put(t_env env, unsigned int x, unsigned int y, t_color color);
+int			key_hook(int keycode, t_env *env);
+
+#endif
---
+++ includes/struct.h
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ple-lez <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/06 02:31:15 by ple-lez           #+#    #+#             */
+/*   Updated: 2017/02/06 02:42:02 by ple-lez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+# include "defines.h"
+
+/* Vector as 3 floats */
+
+typedef struct		s_vec
+{
+	double			x;
+	double			y;
+	double			z;
+	double			w;
+}					t_vec;
+
+/* Quaternion for Rotations
+ *
+ * Component should never be accessed
+ */
+
+typedef struct		s_quat
+{
+	double			r;
+	double			i;
+	double			j;
+	double			k;
+}					t_quat;
+
+/* Rqy structure */
+
+typedef struct		s_ray
+{
+	t_vec			org;
+	t_vec			dir;
+	double			t;
+}					t_ray;
+
+/* Camera structure */
+
+typedef struct		s_cam
+{
+	t_vec			pos;
+	t_quat			*rot;
+	t_mode			control;
+	double			fov;
+}					t_cam;
+
+/* Light structure */
+
+typedef struct		s_lgt
+{
+	t_vec			pos;
+	t_vec			normal;
+	t_vec			hitpnt;
+}					t_lgt;
+
+/* Color structure */
+
+typedef struct		s_color
+{
+	unsigned char	b;
+	unsigned char	g;
+	unsigned char	r;
+	unsigned char	a;
+}					t_color;
+
+/* Object Structures */
+
+typedef struct		s_obj
+{
+	t_vec			pos;
+	t_vec			dir;
+	t_type			type;
+	t_quat			*rot;
+	t_quat			*inv;
+	t_color			rgb;
+	double			radius;
+}					t_obj;
+
+ /* Environement structure */
+
+typedef struct		s_env
+{
+	int				bpp;
+	int				size;
+	int				endian;
+	int				redraw;
+	void			*mlx;
+	void			*win;
+	void			*img;
+	char			*addr;
+	t_lgt			lgt;
+	t_cam			cam;
+	t_obj			*objs;
+}					t_env;
+
+#endif
