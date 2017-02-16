fd6ad269e31c5350dbd17b6194d5f0175f9f82bb
Pierre MARTINEAU <pmartine@e2r3p4.42.fr>
2017-02-16 16:52:19 +0100

    specular_in_progress

--- sources/light.c
+++ sources/light.c
@@ -6,7 +6,7 @@
 /*   By: ple-lez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 20:36:10 by ple-lez           #+#    #+#             */
-/*   Updated: 2017/02/15 10:55:23 by hsabouri         ###   ########.fr       */
+/*   Updated: 2017/02/16 16:49:11 by pmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,28 @@ t_color		apply_lambert(t_color col, t_vec4 coef)
 	return (res);
 }
 
+static double		specular_in_progress(t_obj obj, t_ray ray, t_lgt lgt)
+{
+	t_vec4	dir;
+	t_vec4	tmp;
+	double	spec;
+	dir = vector_sub(lgt.pos, lgt.hitpnt);
+	dir = normalize_vector(dir);
+	tmp = vector_scale(lgt.normal, (2.0 * scalar_product(lgt.normal, dir)));
+	tmp = vector_sub(dir, tmp);
+	if ((spec = scalar_product(tmp, ray.dir)) > 0.0)
+		spec = pow(spec, 30.0);
+	if (spec >= EPSILON && obj.type != PLANE)
+		return (spec);
+	return (0.0);
+}
+
 t_vec4		lambert(t_obj obj, t_ray ray, t_lgt lgt)
 {
 	t_vec4	tmp;
 	t_vec4	dir;
 	double	lamb;
+	double spec;
 	t_vec4	res;
 
 	res = new_vector(lgt.rgb.r, lgt.rgb.g, lgt.rgb.b);
@@ -51,6 +68,7 @@ t_vec4		lambert(t_obj obj, t_ray ray, t_lgt lgt)
 		lamb = 0;
 	if (lamb > 1.0)
 		lamb = 1.0;
+	lamb += specular_in_progress(obj, ray, lgt);
 	res = vector_scale(res, lamb);
 	return (res);
 }
@@ -62,7 +80,9 @@ t_color		lights(t_obj obj, t_ray ray, t_env env, t_color color)
 	t_vec4				coef;
 	t_vec4				tmp;
 	t_color				res;
-
+	double				spec;
+	t_vec4				calc;
+	spec = 0;
 	coef = new_vector(0, 0, 0);
 	res = (t_color) {0, 0, 0, 0};
 	i = 0;
@@ -71,7 +91,7 @@ t_color		lights(t_obj obj, t_ray ray, t_env env, t_color color)
 		if (env.shadow || shadows(env.objs, ray, env.lgt[i], env.last_id))
 		{
 			tmp = lambert(obj, ray, env.lgt[i]);
-			tmp = vector_scale(tmp, 1.0 / 255);	
+			tmp = vector_scale(tmp, 1.0 / 255);
 			coef = vector_add(coef, tmp);
 		}
 		i++;
