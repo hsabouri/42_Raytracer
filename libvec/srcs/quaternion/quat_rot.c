50c3cd3f88a94284907587f22da928ce56193a95
Pierre MARTINEAU <pmartine@e2r10p19.42.fr>
2017-03-01 16:27:16 +0100

    free quat_rot + prec scene

--- libvec/srcs/quaternion/quat_rot.c
+++ libvec/srcs/quaternion/quat_rot.c
@@ -6,7 +6,7 @@
 /*   By: qduperon <qduperon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/14 17:32:20 by qduperon          #+#    #+#             */
-/*   Updated: 2017/02/14 17:39:04 by qduperon         ###   ########.fr       */
+/*   Updated: 2017/03/01 16:20:03 by pmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ t_vec4			quat_rot(t_quat *rot, t_vec4 *vec)
 	tmp = quat_mult(rot, tmp);
 	tmp = quat_mult(tmp, inv);
 	res = (t_vec4){tmp->i, tmp->j, tmp->k, 1};
+	free(tmp);
 	return (res);
 }
