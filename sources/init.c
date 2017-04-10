84833dad96065999d3ceefb702ee7c33e238ec01
Pierre LE LEZ <purplewarlus@gmail.com>
2017-04-10 15:56:29 +0200

    placing function

--- sources/init.c
+++ sources/init.c
@@ -6,7 +6,7 @@
 /*   By: ple-lez <ple-lez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/27 14:50:00 by ple-lez           #+#    #+#             */
-/*   Updated: 2017/04/10 14:38:54 by hsabouri         ###   ########.fr       */
+/*   Updated: 2017/04/10 15:53:54 by ple-lez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ t_env 			init_env(int ac, char **av)
 	env = init_objs_lgts(ac, av, env);
 	env.win = mlx_new_window(env.mlx, env.width, env.height, "RT");
 	create_img(&env.img, env.width, env.height, env.mlx);
-	env.img = init_img(env.img, (t_color){255, 255, 255, 0});
+	env.img = init_img(env.img, (t_color){0, 0, 0, 0});
 	env.objs[env.n_obj - 1] = init_skybox(env);
 	env.redraw = 1;
 	env.drawing = 0;
