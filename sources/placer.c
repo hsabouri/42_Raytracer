bfbeb503356925b13c736f88d0cdb26e6247990a
Pierre LE LEZ <purplewarlus@gmail.com>
2017-04-10 15:58:45 +0200

    select

--- sources/placer.c
+++ sources/placer.c
@@ -6,7 +6,7 @@
 /*   By: ple-lez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 15:43:42 by ple-lez           #+#    #+#             */
-/*   Updated: 2017/04/10 15:52:10 by ple-lez          ###   ########.fr       */
+/*   Updated: 2017/04/10 15:58:33 by ple-lez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,12 @@ int				place_obj(double x, double y, t_env env, t_obj obj)
 	obj.pos = vector_add(ray.org, vector_scale(ray.dir, ray.t));
 	env.objs = add_object(env, obj);
 	return (1);
+}
+
+int				select_obj(double x, double y, t_env env)
+{
+	t_ray		ray;
+
+	ray = init_ray(&env.cam, x, y, &env);
+	return (check_intersections(env.objs, &ray));
 }
