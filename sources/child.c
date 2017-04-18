66bde1fcb0d4062ca2d25fc78dd39cd004b18b5a
Rodolphe BEJOT <rbejot@student.42.fr>
2017-04-18 14:08:00 +0200

    Parsing childs without Type

--- sources/child.c
+++ sources/child.c
@@ -6,26 +6,26 @@
 /*   By: ple-lez <ple-lez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 15:44:43 by ple-lez           #+#    #+#             */
-/*   Updated: 2017/04/18 13:55:05 by rbejot           ###   ########.fr       */
+/*   Updated: 2017/04/18 14:01:22 by rbejot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rt.h"
 
-static t_obj	init_child(t_obj obj)
+static t_obj	init_child(t_obj obj, int n_obj)
 {
 	if (obj.ch_type == CHILD)
 	{
 		obj.chld = (t_obj *)malloc(sizeof(t_obj) * 2);
-		obj.chld[0].name = ft_strdup("child");
-		obj.chld[0].rot = new_quat_null();
-		obj.chld[0].inv = new_quat_null();
-		obj.chld[0].pos = vector_add(obj.pos, (t_vec4){obj.radius, 0, 0, 0});
-		obj.chld[0].type = CONE;
-		obj.chld[0].radius = obj.radius;
-		obj.chld[0].mat = obj.mat;
-		obj.chld[0].chld = NULL;
-		obj.chld[1].type = BACKSLASH;
+		obj.chld[n_obj].name = ft_strdup("child");
+		obj.chld[n_obj].rot = new_quat_null();
+		obj.chld[n_obj].inv = new_quat_null();
+		obj.chld[n_obj].pos = vector_add(obj.pos, (t_vec4){obj.radius, 0, 0, 0});
+		obj.chld[n_obj].type = CONE;
+		obj.chld[n_obj].radius = obj.radius;
+		obj.chld[n_obj].mat = obj.mat;
+		obj.chld[n_obj].chld = NULL;
+		obj.chld[n_obj + 1].type = BACKSLASH;
 	}
 	return (obj);
 }
@@ -39,7 +39,7 @@ void			temp_init(t_env *env, int n_obj)
 	env->objs[n_obj].lmt.axis = (t_vec4){0, 1, 0, 0};
 	env->objs[n_obj].lmt.coef_min = (t_vec4){-0.5, -1, -0.5, 0};
 	env->objs[n_obj].lmt.coef_max = (t_vec4){1, 2, 1, 0};
-	env->objs[n_obj] = init_child(env->objs[n_obj]);
+	env->objs[n_obj] = init_child(env->objs[n_obj], n_obj);
 }
 
 static double	intersect_pipeline(t_ray ray, t_obj *obj)
