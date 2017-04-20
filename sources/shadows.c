6ad7d481e9e92bf8fe54cbe47dd9f6ec09f7cbb8
hsabouri <hsabouri@student.42.fr>
2017-04-20 16:03:36 +0200

    Norme on shadow.c

--- sources/shadows.c
+++ sources/shadows.c
@@ -6,11 +6,28 @@
 /*   By: ple-lez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 15:18:01 by ple-lez           #+#    #+#             */
-/*   Updated: 2017/04/20 13:43:30 by hsabouri         ###   ########.fr       */
+/*   Updated: 2017/04/20 16:02:44 by hsabouri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rt.h>
+
+static double	if_forest(t_obj *objs, t_ray ray, int i, double tmp)
+{
+	if (objs[i].type == MESH)
+		tmp = intersect_mesh(ray, &objs[i]);
+	if (objs[i].type == SPHERE)
+		tmp = intersect_sphere(ray, &objs[i]);
+	else if (objs[i].type == PLANE)
+		tmp = intersect_plane(ray, &objs[i]);
+	else if (objs[i].type == CONE)
+		tmp = intersect_cone(ray, &objs[i]);
+	else if (objs[i].type == CYLINDER)
+		tmp = intersect_cylinder(ray, &objs[i]);
+	else if (objs[i].type == POLYGON)
+		tmp = intersect_polygon(ray, objs[i]);
+	return (tmp);
+}
 
 static double	other_intersect(t_obj *objs, t_ray ray, int id, double max_dis)
 {
@@ -23,23 +40,12 @@ static double	other_intersect(t_obj *objs, t_ray ray, int id, double max_dis)
 	tmp = -1.0;
 	while (objs[i].type != BACKSLASH)
 	{
-			if (objs[i].type == MESH)
-				tmp = intersect_mesh(ray, &objs[i]);
-			if (objs[i].type == SPHERE)
-				tmp = intersect_sphere(ray, &objs[i]);
-			else if (objs[i].type == PLANE)
-				tmp = intersect_plane(ray, &objs[i]);
-			else if (objs[i].type == CONE)
-				tmp = intersect_cone(ray, &objs[i]);
-			else if (objs[i].type == CYLINDER)
-				tmp = intersect_cylinder(ray, &objs[i]);
-			else if (objs[i].type == POLYGON)
-				tmp = intersect_polygon(ray, objs[i]);
-			if ((tmp < t || t <= EPSILON) && tmp > EPSILON && tmp < max_dis)
-			{
-				t = tmp;
-				break ;
-			}
+		tmp = if_forest(objs, ray, i, tmp);
+		if ((tmp < t || t <= EPSILON) && tmp > EPSILON && tmp < max_dis)
+		{
+			t = tmp;
+			break ;
+		}
 		i++;
 	}
 	if (objs[i].type == BACKSLASH || i == id)
